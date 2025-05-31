65cb6aa7ea182aa6b0b18214f7b3a07a4da77614
miaoning <miaoning.exe@gmail.com>
2025-05-31 19:31:47 +0800

    create frontend

---
+++ frontend/vite-project/src/router/index.js
@@ -0,0 +1,15 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'welcome',
+            component: () => import('../views/WelcomeView.vue'),
+            children: []
+        }
+    ]
+})
+
+export default router
